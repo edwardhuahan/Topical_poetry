bac1fbb4ad11f9240a1c0d8f84e51c8dc4f76294
Edward Han <17802344+edwardhuahan@users.noreply.github.com>
2019-08-04 22:32:12 -0600

    Poem gen
    Created poem generator

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ while True:
 os.mkdir(os.path.join(path,data))
 
 random_dir = os.path.join('../',path, data)
-rhyme_dir = os.getcwd()
+current_dir = os.getcwd()
 rhyme_cmd = 'sudo sh '+rhyme_dir+'/run-different-line-numbers.sh '
 rhyme_cmd += topic + ' '
 rhyme_cmd += random_dir+'/poem.fsa '
@@ -24,3 +24,15 @@ rhyme_cmd += '4'
 
 os.system(rhyme_cmd)
 
+root_dir = os.path.join(current_dir, '../')
+poem_cmd = 'python '
+poem_cmd += os.path.join(root_dir, '/py/run_standalone.py ')
+poem_cmd += os.path.join(root_dir, '/models/lyrics.tl.nn ')
+poem_cmd += os.path.join(random_dir, '/source.txt ')
+poem_cmd += os.path.join(random_dir, '/poem.fsa ')
+poem_cmd += os.path.join(random_dir, '/encourage.txt ')
+poem_cmd += os.path.join(random_dir, '/output.txt ')
+poem_cmd += '50 1 '
+poem_cmd += random_dir
+
+print(poem_cmd)
